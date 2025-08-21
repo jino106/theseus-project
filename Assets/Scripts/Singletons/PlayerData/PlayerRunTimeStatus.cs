6fc16750cf3c81d25a47e622554735c477dc0f4f
mentuyu0001 <119647679+mentuyu0001@users.noreply.github.com>
2025-08-21 13:14:06 +0900

    feature(player-run-time-status): PlayerRunTimeStatusの追加

---
+++ Assets/Scripts/Singletons/PlayerData/PlayerRunTimeStatus.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerRunTimeStatus : MonoBehaviour
+{
+    /// <summary>
+    /// パーツによって起こる、特殊行動のプロパティ
+    /// </summary>
+    
+    private bool canDoubleJump = false; // 二段ジャンプ
+    private bool canShootWater = false; // 水を撃つ
+    private bool canThrowKnife = false; // ナイフを投げる
+
+    public bool CanDoubleJump {
+        get {return canDoubleJump;}
+        set {canDoubleJump = value;}
+    }
+
+    public bool CanShootWater {
+        get {return canShootWater;}
+        set {canShootWater = value;}
+    }
+
+    public bool CanThrowKnife {
+        get {return canThrowKnife;}
+        set {canThrowKnife = value;}
+    }
+}
