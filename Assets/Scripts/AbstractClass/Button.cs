ba3b3dd3cb9605a41ade5382041af172950d0d41
mentuyu0001 <119647679+mentuyu0001@users.noreply.github.com>
2025-10-04 22:31:56 +0900

    fix(button): Buttonスクリプトでマウス操作に不具合が出るバグを修正

--- Assets/Scripts/AbstractClass/Button.cs
+++ Assets/Scripts/AbstractClass/Button.cs
@@ -4,11 +4,14 @@ using UnityEngine.EventSystems;
 /// <summary>
 /// UIボタンの抽象クラス
 /// </summary>
-public abstract class Button : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+public abstract class Button : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPointerExitHandler
 {
     /// <summary>
     /// UIが選択されるたびに「カチカチ」と音を流したりする際の親オブジェクト
     /// </summary>
+
+    // カーソルが入ってるか出ているか判定する変数
+    private bool isPointer = false;
 
     // 選択された際の音以外の処理を施す抽象メソッド
     protected virtual void TriggerSelectionEffects() 
@@ -29,14 +32,33 @@ public abstract class Button : MonoBehaviour, ISelectHandler, IPointerEnterHandl
     // キーボードで選択された場合
     public void OnSelect(BaseEventData eventData)
     {
+        if (isPointer)
+        {
+            return;
+        }
+
         TriggerSelectionSounds();
         TriggerSelectionEffects();
+        Debug.Log("キーボード");
     }
 
     // マウスで選択された場合
     public void OnPointerEnter(PointerEventData eventData)
     {
+        // もしこのオブジェクトが既に選択されているなら、何もしない
+        if (EventSystem.current.currentSelectedGameObject == this.gameObject)
+        {
+            return;
+        }
+        isPointer = true;
         TriggerSelectionSounds();
         TriggerSelectionEffects();
+        Debug.Log("マウス");
+    }
+
+    // マウスカーソルがこのUI要素の範囲から「出た」瞬間に呼ばれる
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isPointer = false;
     }
 }
