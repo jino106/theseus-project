e7ea10f665a920e5d1a143dbb81c1eaba8747448
mentuyu0001@gmail.com <mentuyu0001@gmail.com>
2025-11-04 16:58:27 +0900

    fix(button-sound): タイトル画面のボタンSEの修正

---
+++ Assets/Scripts/System/UI/ButtonSound.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+/// ボタンの音を鳴らすスクリプト
+/// </summary>
+public class ButtonSound : Button
+{
+    public override void OnClick()
+    {
+        base.OnClick(); // 決定音を鳴らす
+    }
+}
